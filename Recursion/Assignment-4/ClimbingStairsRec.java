fd7d0ff753d562a46cbf0a760a860598552931f2
Thrisha Rajkumar <85435379+thrisharajkumar@users.noreply.github.com>
2023-06-24 23:36:22 +0530

    Create ClimbingStairsRec.java

---
+++ Recursion/Assignment-4/ClimbingStairsRec.java
@@ -0,0 +1,20 @@
+package Recursion.Assignment_4;
+
+//USING RECURSION
+public class ClimbingStairsRec {
+    static int ClimStairs(int n) {
+        if (n == 1 || n == 0) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        } else {
+            return ClimStairs(n - 2) + ClimStairs(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        int result = ClimStairs(n);
+        System.out.println(result);
+    }
+}
