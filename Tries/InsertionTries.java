3a9ff8d4e29306df1b73b457596a8fe1e6bb86ea
Thrisha Rajkumar <85435379+thrisharajkumar@users.noreply.github.com>
2023-07-15 05:23:28 +0530

    Create InsertionTries.java

---
+++ Tries/InsertionTries.java
@@ -0,0 +1,47 @@
+package Tries;
+
+public class InsertionTries {
+    static class Node {
+        Node[] children;
+        boolean isTerminal;
+
+        public Node() {
+            children = new Node[26];
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+            isTerminal = false;
+        }
+    }
+
+    static Node root = new Node();
+
+    static void insert(String word) {
+        Node current = root;
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if (current.children[index] == null) {
+                current.children[index] = new Node();
+            }
+
+            if (i == word.length() - 1) {
+                current.children[index].isTerminal = true;
+                System.out.println("Word is inserted: " + word);
+            }
+
+            current = current.children[index];
+        }
+    }
+
+    public static void main(String[] args) {
+        String arr[] = { "apple", "cat", "bag" };
+        for (String i : arr) {
+            insert(i);
+        }
+        /*
+         * insert("apple");
+         * insert("cat");
+         * insert("bag");
+         */
+    }
+}
