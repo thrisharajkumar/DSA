2785e19d7418a7bf3536fe766ae3bc269f231247
Thrisha Rajkumar <85435379+thrisharajkumar@users.noreply.github.com>
2023-07-15 15:03:39 +0530

    Create NextSmallerElement.java

---
+++ Stack/NextSmallerElement.java
@@ -0,0 +1,32 @@
+package Stack;
+
+import java.util.Stack;
+
+public class NextSmallerElement {
+    static int[] findNs(int arr[]) {
+        int n = arr.length;
+        int ns[] = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                stack.pop();
+            }
+
+            ns[i] = stack.isEmpty() ? -1 : arr[stack.peek()];
+
+            stack.push(i);
+        }
+
+        return ns;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 50, 60, 50, 40, 95, 80 };
+        int ans[] = findNs(arr);
+
+        for (int i : ans) {
+            System.out.println(i + " ");
+        }
+    }
+}
