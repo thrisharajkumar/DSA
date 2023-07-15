73f107658f38f8eb207bee89a9e5e048ca990b14
Thrisha Rajkumar <85435379+thrisharajkumar@users.noreply.github.com>
2023-07-15 07:21:17 +0530

    Create SuffixTries.java

---
+++ Tries/SuffixTries.java
@@ -0,0 +1,47 @@
+package Tries;
+
+public class SuffixTries {
+
+    static class Node {
+        Node[] children;
+        boolean isTerminal;
+
+        public Node() {
+            children = new Node[26];
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+            isTerminal = false;
+        }
+    }
+
+    static Node root = new Node();
+
+    static void insert(String word) {
+        Node current = root;
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if (current.children[index] == null) {
+                current.children[index] = new Node();
+            }
+
+            if (i == word.length() - 1) {
+                current.children[index].isTerminal = true;
+                System.out.println("Word is inserted: " + word);
+            }
+
+            current = current.children[index];
+        }
+    }
+
+    static void insertSuffix(String word) {
+        for (int i = 0; i < word.length(); i++) {
+            String suffix = word.substring(i);
+            insert(suffix);
+        }
+    }
+
+    public static void main(String[] args) {
+        insertSuffix("Brain");
+    }
+}
