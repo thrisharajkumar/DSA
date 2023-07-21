561282d9c3c511fdf3461e7adcda5ddb00becd25
Thrisha Rajkumar <85435379+thrisharajkumar@users.noreply.github.com>
2023-07-22 03:52:49 +0530

    Create getIntersectionNode.java

---
+++ Linked_Lists/getIntersectionNode.java
@@ -0,0 +1,32 @@
+package Linked_List;
+
+public class getIntersectionNode {
+
+    /* Definition for singly-linked list. */
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a != b) {
+            if (a == null) {
+                a = headB;
+            } else
+                a = a.next;
+            if (b == null) {
+                b = headA;
+            } else
+                b = b.next;
+        }
+        return a;
+    }
+
+}
